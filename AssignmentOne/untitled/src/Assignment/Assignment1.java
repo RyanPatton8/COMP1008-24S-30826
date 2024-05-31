b46fd62fc09769f09a8abf4c2fa6ba8c15b8222b
RyanPatton8 <116020479+RyanPatton8@users.noreply.github.com>
2024-05-31 08:45:03 -0400

    Fixed task 1 asking for extra element

--- AssignmentOne/untitled/src/Assignment/Assignment1.java
+++ AssignmentOne/untitled/src/Assignment/Assignment1.java
@@ -8,21 +8,25 @@ public class Assignment1 {
     }
 
     static void task1() {
+        //create instance of scanner object
         Scanner sc = new Scanner(System.in);
+        //ask player how many elements theyd like to create
         System.out.println("How many elements would you like to create?");
-
+        //intake int
         int amount = sc.nextInt();
-
+        //use int to create object array of that size
         Object[] array = new Object[amount];
+        //bool to end future while loop
         boolean allEntered = false;
+        //index to keep track of the loop
         int index = 0;
 
+        //ask player to populate area being sure to alter data type depending
+        //on if it matches the criteria of said data type
         while(!allEntered) {
-            System.out.println("please enter element " + index + " of the array");
+            System.out.println("please enter element " + index + " into the array");
             int input = sc.nextInt();
-            if(index > array.length - 1) {
-                allEntered = true;
-            } else if(input == 1) {
+            if(input == 1) {
                 array[index++] = true;
             } else if(input == 0){
                 array[index++] = false;
@@ -33,7 +37,18 @@ public class Assignment1 {
             } else {
                 array[index++] = input;
             }
+            if(index > array.length - 1) {
+                allEntered = true;
+                System.out.println("allentered");
+            }
         }
+
+        //this is just here for you to test it. It doesn't say to print the results in the instructions
+        /*
+        for (Object item : array) {
+            System.out.println(item);
+        }
+        */
     }
 
     static void task2(int[] arr) {
@@ -47,7 +62,7 @@ public class Assignment1 {
         int index = 0;
 
         for (int item : arr) {
-            System.out.println("please enter element " + index + " of the array");
+            System.out.println("please enter element " + index + " into the array");
             arr[index++] = sc.nextInt();
         }
         task2(arr);
