ab3f61e4622fcf41ca5ce21dc4b91da0daf88879
Ryan Patton <ryanpatton173@gmail.com>
2024-07-25 09:58:39 -0400

    Vehicle and truck mostly done

--- AssignmentThree/src/assignment3/Truck.java
+++ AssignmentThree/src/assignment3/Truck.java
@@ -1,4 +1,59 @@
 package assignment3;
 
-public class Truck {
+public class Truck extends Vehicle {
+    public byte seats = 5;
+    // I'm measuring in cubic feet some vehicles get up to 144 so byte wasn't an option
+    // plus I already used it
+    protected short trunkSpace = 80;
+
+    public Truck() {}
+
+    public Truck(String color, byte numberOfDoors, boolean gasPowered) {
+        this.color = color;
+        this.numberOfDoors = numberOfDoors;
+        this.setGasPowered(gasPowered);
+    }
+
+    public Truck(byte numberOfDoors, boolean gasPowered, String color) {
+        this.color = color;
+        this.numberOfDoors = numberOfDoors;
+        this.setGasPowered(gasPowered);
+    }
+
+    public Truck(boolean gasPowered, String color, byte numberOfDoors) {
+        this.color = color;
+        this.numberOfDoors = numberOfDoors;
+        this.setGasPowered(gasPowered);
+    }
+
+    public Truck(boolean gasPowered, byte numberOfDoors) {
+        this.numberOfDoors = numberOfDoors;
+        this.setGasPowered(gasPowered);
+    }
+
+    public Truck(String color, byte numberOfDoors, boolean gasPowered, byte seats) {
+        this.color = color;
+        this.numberOfDoors = numberOfDoors;
+        this.setGasPowered(gasPowered);
+        this.seats = seats;
+    }
+
+    public Truck(String color, byte numberOfDoors, boolean gasPowered, short trunkSpace) {
+        this.color = color;
+        this.numberOfDoors = numberOfDoors;
+        this.setGasPowered(gasPowered);
+        this.trunkSpace = trunkSpace;
+    }
+
+    public Truck(String color, byte numberOfDoors, boolean gasPowered, byte seats, short trunkSpace) {
+        this.color = color;
+        this.numberOfDoors = numberOfDoors;
+        this.setGasPowered(gasPowered);
+        this.seats = seats;
+        this.trunkSpace = trunkSpace;
+    }
+    @Override
+    public boolean isEcoFriendly() {
+        return !gasPowered && numberOfDoors == 2 && trunkSpace == 0;
+    }
 }
--- AssignmentThree/src/assignment3/Vehicle.java
+++ AssignmentThree/src/assignment3/Vehicle.java
@@ -1,4 +1,61 @@
 package assignment3;
 
 public class Vehicle {
+    public String color = "White";
+    // you may wonder what car has the most doors.
+    // don't worry I took the liberty of looking it up, the Toyota Tundrasine Limo Pickup with 8 total doors
+    // what a world.
+    protected byte numberOfDoors = 4;
+    private boolean gasPowered = true;
+
+
+    public boolean isGasPowered() {
+        return gasPowered;
+    }
+
+    public void setGasPowered(boolean gasPowered) {
+        this.gasPowered = gasPowered;
+    }
+
+    public boolean isEcoFriendly() {
+        return !gasPowered && numberOfDoors == 2;
+    }
+
+    public Vehicle() {}
+
+    public Vehicle(String color, byte numberOfDoors, boolean gasPowered) {
+        this.color = color;
+        this.numberOfDoors = numberOfDoors;
+        this.gasPowered = gasPowered;
+    }
+
+    public Vehicle(byte numberOfDoors, boolean gasPowered, String color) {
+        this.color = color;
+        this.numberOfDoors = numberOfDoors;
+        this.gasPowered = gasPowered;
+    }
+
+    public Vehicle(boolean gasPowered, String color, byte numberOfDoors) {
+        this.color = color;
+        this.numberOfDoors = numberOfDoors;
+        this.gasPowered = gasPowered;
+    }
+
+    public Vehicle(boolean gasPowered, byte numberOfDoors) {
+        this.numberOfDoors = numberOfDoors;
+        this.gasPowered = gasPowered;
+    }
+
+//    4) Override (not Overload) the equals() method so it evaluates two Vehicle objects and returns true
+//            if and only if (iff) the two Vehicle objects are equal is value (2 marks)
+    @Override
+    public boolean equals(Object obj) {
+        if (!(obj instanceof Vehicle compare)) {
+            return false;
+        } else {
+            return this.color.equals(compare.color) && this.numberOfDoors == compare.numberOfDoors && this.gasPowered == compare.gasPowered;
+        }
+    }
+
+//    5) Override the toString method to summarize all instance variables of the class (1 mark)
 }
